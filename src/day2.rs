d1102bfb81e45a43dd411a15357f08b50cd74b47
Ninjani <48680156+Ninjani@users.noreply.github.com>
2020-12-02 13:50:50 +0100

    switching to parse-display

--- src/day2.rs
+++ src/day2.rs
@@ -1,60 +1,51 @@
-use itertools::Itertools;
+use parse_display::{Display, FromStr};
 use rayon::prelude::*;
 
 /// cargo aoc bench results
-/// Generator Day2/(default) time:   [430.79 us 444.97 us 460.47 us]
+/// Generator Day2/(default) time:   [977.92 us 1.0102 ms 1.0453 ms]
 /// Day2 - Part1/(default)  time:   [8.0064 us 8.2369 us 8.5390 us]
 /// Day2 - Part2/(default)  time:   [2.0522 us 2.0620 us 2.0735 us]
 
+#[derive(Display, FromStr, PartialEq, Debug)]
+#[display("{n1}-{n2} {letter}: {password}")]
 pub struct Policy {
     n1: usize,
     n2: usize,
-    letter: u8,
+    letter: char,
+    password: String,
 }
 
 #[aoc_generator(day2)]
-pub fn generate(input: &str) -> Vec<(Policy, String)> {
+pub fn generate(input: &str) -> Vec<Policy> {
     input
         .par_lines()
-        .map(|line| {
-            let (policy, password) = line.trim().split(": ").collect_tuple().unwrap();
-            let (numbers, letter) = policy.split(' ').collect_tuple().unwrap();
-            let (n1, n2) = numbers
-                .split('-')
-                .map(|n| n.parse::<usize>().unwrap())
-                .collect_tuple()
-                .unwrap();
-            (
-                Policy {
-                    n1,
-                    n2,
-                    letter: letter.as_bytes()[0],
-                },
-                password.to_owned(),
-            )
-        })
+        .map(|line| line.parse().unwrap())
         .collect()
 }
 
 #[aoc(day2, part1)]
-pub fn solve_part1(input: &[(Policy, String)]) -> usize {
+pub fn solve_part1(input: &[Policy]) -> usize {
     input
         .iter()
-        .filter(|(policy, password)| {
-            let count = password.bytes().filter(|c| c == &policy.letter).count();
+        .filter(|policy| {
+            let count = policy
+                .password
+                .bytes()
+                .filter(|c| c == &(policy.letter as u8))
+                .count();
             count >= policy.n1 && count <= policy.n2
         })
         .count()
 }
 
 #[aoc(day2, part2)]
-pub fn solve_part2(input: &[(Policy, String)]) -> usize {
+pub fn solve_part2(input: &[Policy]) -> usize {
     input
         .iter()
-        .filter(|(policy, password)| {
-            let chars = password.as_bytes();
-            (chars.get(policy.n1 - 1) == Some(&policy.letter))
-                != (chars.get(policy.n2 - 1) == Some(&policy.letter))
+        .filter(|policy| {
+            let chars = policy.password.as_bytes();
+            (chars.get(policy.n1 - 1) == Some(&(policy.letter as u8)))
+                != (chars.get(policy.n2 - 1) == Some(&(policy.letter as u8)))
         })
         .count()
 }
