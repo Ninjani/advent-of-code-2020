194018f6d9130b3a2ca8ee1332b1b07ab46ecc51
Ninjani <48680156+Ninjani@users.noreply.github.com>
2020-12-05 13:02:44 +0100

    day 5 cleanup

--- src/day5.rs
+++ src/day5.rs
@@ -1,25 +1,27 @@
-/// cargo aoc bench results:
-/// Day5 - Part1/(default)  time:   [8.1343 us 8.2398 us 8.3567 us]                                   
-/// Day5 - Part2/(default)  time:   [5.9458 us 6.0279 us 6.1174 us]
+/// cargo aoc bench results (includes parsing):
+/// Day5 - Part1/(default)  time:   [6.5198 us 6.6642 us 6.8384 us]                                                                                               
+/// Day5 - Part2/(default)  time:   [6.8722 us 6.9843 us 7.1139 us]                                    
 
 #[inline(always)]
 fn get_id(seat: &[u8]) -> u32 {
-    seat.iter().fold(0, |acc, &b| {
-        acc * 2 + if b == b'F' || b == b'L' { 0 } else { 1 } as u32
+    seat.iter().take(10).fold(0, |acc, &b| {
+        (acc << 1) | if b == b'F' || b == b'L' { 0 } else { 1 }
     })
 }
 
 #[aoc(day5, part1)]
 pub fn solve_part1(input: &[u8]) -> u32 {
-    (0..input.len() / 11 + 1).fold(0, |max, i| get_id(&input[i * 11..i * 11 + 10]).max(max))
+    input.chunks(11).fold(0, |max, seat| max.max(get_id(&seat)))
 }
 
 #[aoc(day5, part2)]
 pub fn solve_part2(input: &[u8]) -> u32 {
-    let (sum, min, max) = (0..input.len() / 11 + 1).fold((0, 1024, 0), |(sum, min, max), i| {
-        let id = get_id(&input[i * 11..i * 11 + 10]);
-        (sum + id, id.min(min), id.max(max))
-    });
+    let (sum, min, max) = input
+        .chunks(11)
+        .fold((0, 1024, 0), |(sum, min, max), seat| {
+            let id = get_id(&seat);
+            (sum + id, min.min(id), max.max(id))
+        });
     (min..max + 1).sum::<u32>() - sum
 }
 
